e733b84901a51718a00ef67c6475e03d77409e69
中島駿介 <nakajimashunsuke@nakajimashunsukenoMacBook-Pro.local>
2019-08-13 19:17:02 +0900

    映画ランキングデザイン変更

--- src/components/EpicSpinners.vue
+++ src/components/EpicSpinners.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" style="display: flex; justify-content: center; align-items: center; height:400px;">
-    <orbit-spinner :animation-duration="1200" :size="55" color="#2196F3" />
+    <orbit-spinner :animation-duration="1200" :size="55" color="#FF9800" />
   </div>
 </template>
 
--- src/router.js
+++ src/router.js
@@ -12,12 +12,9 @@ export default new Router({
       component: () => import( /* webpackChunkName: "about" */ './views/Home')
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import( /* webpackChunkName: "about" */ './views/About.vue')
+      path: '/movieranking',
+      name: 'movieranking',
+      component: () => import( /* webpackChunkName: "about" */ './views/MovieRanking')
     }
   ]
 })
